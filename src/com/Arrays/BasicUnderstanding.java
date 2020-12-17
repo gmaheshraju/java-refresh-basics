961ef081089312a9f355e008b523a566fc311abd
Mahesh Guntumadugu <g.maheshlearn@outlook.com>
2020-12-17 10:42:16 +0530

    arrays Basics
    Arrays

---
+++ src/com/Arrays/BasicUnderstanding.java
@@ -0,0 +1,44 @@
+package com.Arrays;
+
+public class BasicUnderstanding {
+	
+	
+	public static void main(String[] args) {
+		
+		// declare an array
+		
+		int[] intArray;
+		
+		//declare and initialize
+		int[] intArraywithInitialization = new int [5];
+		
+		System.out.println(intArraywithInitialization.length);
+		
+		
+		intArraywithInitialization[0]=10;
+		intArraywithInitialization[1]=20;
+		intArraywithInitialization[2]=30;
+		intArraywithInitialization[3]=40;
+		intArraywithInitialization[4]=50;
+		
+		//accessing array
+		
+		for (int i=0 ; i<intArraywithInitialization.length ; i++) {
+			System.out.println(intArraywithInitialization[i]);
+		}
+		
+		//two dimensional arrays
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+	
+	
+
+}
